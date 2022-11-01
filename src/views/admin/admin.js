e79c8a915949efcf7f26fa8444a6fd13147dfef2
yang <ysotgood@gmail.com>
2022-11-01 13:46:34 +0900

    html,admin,router 수정

--- src/views/admin/admin.js
+++ src/views/admin/admin.js
@@ -6,19 +6,29 @@ const productSize = document.querySelector("#productSize");
 const productImg = document.querySelector("#productImg");
 const submitAddProduct = document.querySelector("#submitAddProduct");
 
-submitAddProduct.addEventListener("click", handleSubmit);
+submitAddProduct.addEventListener("submit", handleSubmit);
 
-async function handleSubmit(e) {
+// async function handleSubmit(e) {
+//   e.preventDefault();
+//   const name = productName.value;
+//   const dsc = productDsc.value;
+//   const price = productPrice.value;
+//   const size = productSize.value;
+//   const img = productImg.value;
+//   try {
+//     const data = { name, dsc, price, size, img };
+//     const result = await Api.post("/api/admin", data);
+//   } catch (err) {
+//     console.log(err.stack);
+//   }
+// }
+function handleSubmit(e) {
   e.preventDefault();
   const name = productName.value;
   const dsc = productDsc.value;
   const price = productPrice.value;
   const size = productSize.value;
   const img = productImg.value;
-  try {
-    const data = { name, dsc, price, size, img };
-    const result = await Api.post("/api/admin", data);
-  } catch (err) {
-    console.log(err.stack);
-  }
+  const data = { name, dsc, price, size, img };
+  console.log(data);
 }
