54845a0a98dcddb6710f18bfc43945fff651c82e
유진 채 <wiseidea01@gmail.com>
2022-11-01 09:05:02 +0000

    Revert "Merge branch 'Be-feature-model-schema' into 'master'"
    This reverts merge request !11

--- src/db/models/cart-model.js
+++
@@ -1,6 +0,0 @@
-import { model } from "mongoose";
-import { CartSchema } from "../schemas/cart-schema";
-
-const Cart = model("cart", CartSchema);
-
-export { Cart };
--- src/db/models/orderDetails-model.js
+++
@@ -1,6 +0,0 @@
-import { model } from "mongoose";
-import { OrderDetailSchema } from "../schemas/orderDetails-schema";
-
-const OrderDetails = model("ordersDetails", OrderDetailSchema);
-
-export { OrderDetails };
