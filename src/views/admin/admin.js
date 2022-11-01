46a2d4af628288dd3371571115a0f40264f842f8
yang <ysotgood@gmail.com>
2022-11-01 16:14:55 +0900

    FEATURE-상품추가 핸들러 등록

--- src/views/admin/admin.js
+++ src/views/admin/admin.js
@@ -4,9 +4,7 @@ const productDsc = document.querySelector("#productDsc");
 const productPrice = document.querySelector("#productPrice");
 const productSize = document.querySelector("#productSize");
 const productImg = document.querySelector("#productImg");
-const submitAddProduct = document.querySelector("#submitAddProduct");
-
-submitAddProduct.addEventListener("submit", handleSubmit);
+const submitAddProduct = document.querySelector("#porductAddForm");
 
 // async function handleSubmit(e) {
 //   e.preventDefault();
@@ -17,18 +15,24 @@ submitAddProduct.addEventListener("submit", handleSubmit);
 //   const img = productImg.value;
 //   try {
 //     const data = { name, dsc, price, size, img };
-//     const result = await Api.post("/api/admin", data);
+//     const result = await Api.post("/admin", data);
 //   } catch (err) {
 //     console.log(err.stack);
 //   }
+//   console.log(result);
 // }
-function handleSubmit(e) {
+
+function handleSubmitAddProduct(e) {
   e.preventDefault();
   const name = productName.value;
   const dsc = productDsc.value;
   const price = productPrice.value;
   const size = productSize.value;
-  const img = productImg.value;
-  const data = { name, dsc, price, size, img };
+  const imgurl = productImg.value;
+
+  const data = { name, dsc, price, size, imgurl };
+
   console.log(data);
 }
+
+submitAddProduct.addEventListener("submit", handleSubmitAddProduct);
