5d82f1790c36f03b5cb25049d79b16811025d176
EUNCHAE KIM <112812483+eunbbori@users.noreply.github.com>
2022-11-02 14:42:29 +0900

    FEATURE  - orders-schema-> order-schema ,  orders-model -> order-model 로 통일  (파일명 복수 -> 단수)

--- src/db/models/order-model.js
+++ src/db/models/order-model.js
@@ -1,5 +1,5 @@
 import { model } from "mongoose";
-import { OrderSchema } from "../schemas/orders-schema";
+import { OrderSchema } from "../schemas/order-schema";
 
 const Orders = model("orders", OrderSchema);
 
