b90ed32e24f9b428500ba69dab2eeebd7b7859cd
Kim Chansoo <96669858+cskim9410@users.noreply.github.com>
2022-11-03 01:48:58 +0900

    pagination, brand category hover
    pagination, brand category hover
    
    - 페이지네이션 기능 작성중, 페이지에 그려내는 거까지 해둠 클릭했을 때 서버에 요청보내도록 수정 필요
    
    - 브랜드에 마우스올렸을 때 카테고리 나타나도록 이벤트 추가

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -3,97 +3,129 @@
 // 코드 예시를 남겨 두었습니다.
 
 // import * as Api from "/api.js";
-// import { randomId } from "/useful-functions.js";
+/**
+ * @description 초기 실행 함수
+ */
+async function init() {
+  const {currentPage, lastPage, totalCount, items} = await getServerData();
+  
+  makeProductList()
+  pagination({currentPage, lastPage, totalCount})
 
-// 요소(element), input 혹은 상수
-const landingDiv = document.querySelector("#landingDiv");
-const greetingDiv = document.querySelector("#greetingDiv");
-
-
-addAllElements();
-addAllEvents();
-
-// html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
-async function addAllElements() {
-  insertTextToLanding();
-  insertTextToGreeting();
 }
 
-// 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
-function addAllEvents() {
-  landingDiv.addEventListener("click", alertLandingText);
-  greetingDiv.addEventListener("click", alertGreetingText);
+
+
+function getServerData() { //test용 json파일요청
+  const res = fetch('./test.json')
+    .then(res => res.json())
+    .then(item => item)
+    return res; 
 }
 
-function insertTextToLanding() {
-  landingDiv.insertAdjacentHTML(
-    "beforeend",
-    `
-      <h2>n팀 쇼핑몰의 랜딩 페이지입니다. 자바스크립트 파일에서 삽입되었습니다.</h2>
-    `
-  );
-}
 
-function insertTextToGreeting() {
-  greetingDiv.insertAdjacentHTML(
-    "beforeend",
-    `
-      <h1>반갑습니다! 자바스크립트 파일에서 삽입되었습니다.</h1>
-    `
-  );
-}
 
-function alertLandingText() {
-  alert("n팀 쇼핑몰입니다. 안녕하세요.");
-}
 
-function alertGreetingText() {
-  alert("n팀 쇼핑몰에 오신 것을 환영합니다");
-}
+
 
 async function getDataFromApi() {
   // 예시 URI입니다. 현재 주어진 프로젝트 코드에는 없는 URI입니다.
   const data = await Api.get("/api/user/data");
-  const random = randomId();
 
   console.log({ data });
-  console.log({ random });
 }
 
-async function showProduct() { //test용 json파일요청
-  return await fetch('./test.json')
-    .then(res => res.json())
+
+function pagination({currentPage, lastPage, totalCount}) {
+ 
+  if (totalCount <= 20) return; 
+
+	var pageGroup = Math.ceil(currentPage / 10);
+	
+	var last = pageGroup * 10;
+	if (last > lastPage) last = lastPage;
+	var first = last - (10 - 1) <= 0 ? 1 : last - (10 - 1);
+
+	const fragmentPage = document.createDocumentFragment();
+  if (pageGroup > 1) {
+	  var allpreli = document.createElement('li');
+	  allpreli.insertAdjacentHTML("beforeend", `<a href='#js-bottom' id='allprev'>&lt;&lt;</a>`);
+	
+	  var preli = document.createElement('li');
+	  preli.insertAdjacentHTML("beforeend", `<a href='#js-ottom' id='prev'>&lt;</a>`);
+	
+	  fragmentPage.appendChild(allpreli);
+	  fragmentPage.appendChild(preli);
+	}
+
+  for (let i = first; i <= last; i++) {
+	  const li = document.createElement("li");
+	  li.insertAdjacentHTML("beforeend", `<a href='#js-bottom' id='page-${i}' data-num='${i}'>${i}</a>`);
+    li.addEventListener('click', () => {alert(i)})
+	  fragmentPage.appendChild(li);
+    
+  }
+
+  if (last < lastPage) {
+	  var allendli = document.createElement('li');
+	  allendli.insertAdjacentHTML("beforeend", `<a href='#js-bottom'  id='allnext'>&gt;&gt;</a>`);
+	
+	  var endli = document.createElement('li');
+	  endli.insertAdjacentHTML("beforeend", `<a  href='#js-bottom'  id='next'>&gt;</a>`);
+	
+	  fragmentPage.appendChild(endli);
+	  fragmentPage.appendChild(allendli);
+  }
+
+    document.getElementById('js-pagination').appendChild(fragmentPage);
+		// 페이지 목록 생성0
+    
 }
 
 function makeProductList() {  // 각 data마다 html을 생성하여 data를 삽입
-  showProduct().then((data) => {
-    data.forEach((item) => {
-      const brandName = item.brandName;
-      const productName = item.productName;
-      const price = item.price;
-      const productId = item.productId;
-      selectElement('.productList').insertAdjacentHTML(
-        'beforeend',
-        `<li class="product" id="${productId}">
-        <a href="/product/:${productId}">
-        <img src="../elice-rabbit.png">
-        <div>
-        <p class="brandName">브랜드</p>
-        <p class="productName">상품명</p>
-        <p class="price">가격</p>
-        </div>
-        </a>
-        </li>`
-      )
-        selectElement(`#${productId} > a > div > .brandName`).innerHTML = brandName
-        selectElement(`#${productId} > a > div > .productName`).innerHTML = productName
-        selectElement(`#${productId} > a > div > .price`).innerHTML = price
-    })
-  })
+  // showProduct().then((data) => {
+  //   data.items.forEach((item) => {
+  //     const brandName = item.brandName;
+  //     const productName = item.productName;
+  //     const price = item.price;
+  //     const productId = item.productId;
+  //     selectElement('.productList').insertAdjacentHTML(
+  //       'beforeend',
+  //       `<li class="product" id="${productId}">
+  //       <a href="/product/:${productId}">
+  //       <img src="../elice-rabbit.png">
+  //       <div>
+  //       <p class="brandName">브랜드</p>
+  //       <p class="productName">상품명</p>
+  //       <p class="price">가격</p>
+  //       </div>
+  //       </a>
+  //       </li>`
+  //     )
+  //       selectElement(`#${productId} > a > div > .brandName`).innerHTML = brandName
+  //       selectElement(`#${productId} > a > div > .productName`).innerHTML = productName
+  //       selectElement(`#${productId} > a > div > .price`).innerHTML = price
+  //   })
+  // })
 }
 
 function selectElement(selector) {    //selector에 선택자를 포함한 str을 넣어줘서 html요소를 반환한다.
   return document.querySelector(selector)
 }
 
-makeProductList()
+
+
+
+selectElement('#brand').addEventListener('mouseenter', () => {
+  console.log('a')
+  selectElement('.categoryList').classList.remove('hidden')
+})
+selectElement('.categoryList').addEventListener('mouseleave', () => {
+  console.log('a')
+  selectElement('.categoryList').classList.add('hidden')
+})
+
+
+
+
+init();
