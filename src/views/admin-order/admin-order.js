a1b4ea9e413daa2855500c3d59ffe1bb912ef723
yang <ysotgood@gmail.com>
2022-11-02 16:10:36 +0900

    관리자 페이지-주문관련 조회 수정 v2

--- src/views/admin-order/admin-order.js
+++ src/views/admin-order/admin-order.js
@@ -1,13 +1,35 @@
 const ordersContainer = document.querySelector(".orders-container");
 const btnRef = document.querySelector("#orderRefBtn");
+const orderCount = document.querySelector("#ordersCount");
+const prepareCount = document.querySelector("#prepareCount");
+const deliveryCount = document.querySelector("#deliveryCount");
+const completeCount = document.querySelector("#completeCount");
+const addCommas = (n) => {
+  return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
 
 const userOrderList = async function (e) {
   e.preventDefault();
   try {
     const res = await fetch("./product.json");
-    const result = await res.json();
-    console.log(result);
-    result.map((e) => {
+    const results = await res.json();
+    const summary = {
+      ordersCount: 0,
+      prepareCount: 0,
+      deliveryCount: 0,
+      completeCount: 0,
+    };
+    for (const result of results) {
+      const { userId, productName, account, productPrice, orderState } = result;
+      if (orderState === "상품 준비중") {
+        summary.prepareCount += 1;
+      } else if (orderState === "상품 배송중") {
+        summary.deliveryCount += 1;
+      } else if (orderState === "배송완료") {
+        summary.completeCount += 1;
+      }
+    }
+    results.map((e) => {
       ordersContainer.insertAdjacentHTML(
         "beforeend",
         `<div class="columns orders-item">
@@ -19,18 +41,21 @@ const userOrderList = async function (e) {
             <select id="statusSelectBox-${e.userId}">
              <option 
               class="has-background-danger-light has-text-danger"
-              value=${e.orderState}>
-              ${e.orderState}
+              ${e.orderState === "상품 준비중" ? "selected" : ""}
+              value="상품 준비중">
+              상품 준비중
               </option>
               <option 
               class="has-background-primary-light has-text-primary"
-              value=${e.orderState}>
-              ${e.orderState}
+              ${e.orderState === "상품 준비중" ? "selected" : ""}
+              value="상품 배송중">
+              상품 배송중
               </option>
               <option 
               class="has-background-grey-light"
-              value=${e.orderState}>
-              ${e.orderState}
+              ${e.orderState === "상품 준비중" ? "selected" : ""}
+              value="배송완료">
+              배송완료
               </option>
             </select>
           </div>
@@ -47,6 +72,10 @@ const userOrderList = async function (e) {
       </div>`
       );
     });
+    orderCount.innerText = addCommas(results.length);
+    prepareCount.innerText = addCommas(summary.prepareCount);
+    deliveryCount.innerText = addCommas(summary.deliveryCount);
+    completeCount.innerText = addCommas(summary.completeCount);
   } catch (err) {
     console.log(err);
   }
