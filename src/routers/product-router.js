7c96503112dab37648e5b4bfaad7074b2d444f59
yang <ysotgood@gmail.com>
2022-11-03 09:16:41 +0900

    관리자 페이지- 카테고리 삭제 함수 추가

--- src/routers/product-router.js
+++ src/routers/product-router.js
@@ -6,34 +6,43 @@ import { productService } from "../services";
 const productRouter = Router();
 
 // 상품 저장 api
-productRouter.post('/', loginRequired, async function (req, res, next) {
-    try {
-      // application/json 설정을 프론트에서 안 하면, body가 비어 있게 됨.
-      if (is.emptyObject(req.body)) {
-        throw new Error(
-         "headers의 Content-Type을 application/json으로 설정해주세요"
-        );
-      }
-
-      // req (request)의 body 에서 데이터 가져오기
-      const { code, brand, name, imageUrl, content, category, size, price, stock } = req.body;
-      const newProduct = await productService.addProduct({
-        code,
-        brand,
-        name,
-        imageUrl,
-        content,
-        category,
-        size,
-        price,
-        stock
-      });
-
-      res.status(200).json(newProduct);
-    } catch (error) {
-      next(error);
+productRouter.post("/", loginRequired, async function (req, res, next) {
+  try {
+    // application/json 설정을 프론트에서 안 하면, body가 비어 있게 됨.
+    if (is.emptyObject(req.body)) {
+      throw new Error(
+        "headers의 Content-Type을 application/json으로 설정해주세요"
+      );
     }
+
+    // req (request)의 body 에서 데이터 가져오기
+    const {
+      code,
+      brand,
+      name,
+      imageUrl,
+      content,
+      category,
+      size,
+      price,
+      stock,
+    } = req.body;
+    const newProduct = await productService.addProduct({
+      code,
+      brand,
+      name,
+      imageUrl,
+      content,
+      category,
+      size,
+      price,
+      stock,
+    });
+
+    res.status(200).json(newProduct);
+  } catch (error) {
+    next(error);
   }
-);
+});
 
 export { productRouter };
--- src/views/admin-category/admin-category.js
+++ src/views/admin-category/admin-category.js
@@ -1,5 +1,8 @@
 const categoryAddForm = document.querySelector("#categoryAddForm");
 const categoryAddInput = document.querySelector("#categoryAdd");
+const categoryDeleteForm = document.querySelector("#categoryDeleteForm");
+const categorySelectBox = document.querySelector("#brandSelect");
+
 function categoryAddFormHandler(e) {
   e.preventDefault();
   const category = categoryAddInput.value;
@@ -22,5 +25,22 @@ function categoryAddFormHandler(e) {
   // }
   categoryAddForm.reset();
 }
-
+function categoryDeleteFormHandler(e) {
+  e.preventDefault();
+  const brandSelectValue = categorySelectBox.value;
+  // try {
+  //   await fetch(`/api/category/${brandSelectValue}`, {
+  //     method: "DELETE",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Authorization:
+  //         "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MzYwYjQ4MWJjMGZiY2I1YWFhNDYxMmMiLCJyb2xlIjoiYmFzaWMtdXNlciIsImlhdCI6MTY2NzI4MjIwNH0.pAegQIKEaZmGFznaEablnGuF-1iDFLZs9OgmW4EYFbE",
+  //     },
+  //   });
+  // categoryDeleteForm.reset();
+  // } catch (err) {
+  //   console.log(err.stack);
+  // }
+}
 categoryAddForm.addEventListener("submit", categoryAddFormHandler);
+categoryDeleteForm.addEvesntListener("submit", categoryDeleteFormHandler);
