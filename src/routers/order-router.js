896657d993c9a30bc7ad21f4f6513d255df0a50e
yj0903 <wiseidea01@gmail.com>
2022-11-04 14:22:08 +0900

    (주문기능) 조회,등록,수정,삭제 / jsdoc 설명

--- src/routers/order-router.js
+++ src/routers/order-router.js
@@ -1,23 +1,53 @@
-
 /**
- * @class order-router
+ * @function GET: /api/order/admin
+ * @author 채유진
+ * @description 관리자가 모든 고객의 주문목록을 조회할 수 있습니다.
+ * @return {object} - code 200, orderList
  */
 import { Router } from "express";
+import is from "@sindresorhus/is";
 import { orderService } from "../services";
-
-/**
- * express module
- * @const
- */
 const orderRouter = Router();
 
-
+/**
+ * @function GET: /api/order/admin
+ * @author 채유진
+ * @description 관리자가 모든 고객의 주문목록을 조회할 수 있습니다.
+ * @return {object} - code 200, orderList
+ */
+orderRouter.get('/admin', async (req, res, next) => {
+    try{
+        const orders = await orderService.getOrderAll();
+        res.status(200).json(orders);
+    } catch(err){
+        next(err)
+    }
+})
 
 /**
- * get, api/order/userId
- * @param userId
+ * @function GET: /api/order?id=orderId
+ * @author 채유진
+ * @description 주문 id로 고객의 주문목록을 조회할 수 있습니다.
+ * @param orderId - orderId ** [req.query]
+ * @return {object} - code 200, orderList
  */
-// 사용자 주문조회 GET: /api/order/userId
+orderRouter.get('/', async (req, res, next) => {
+    try {
+        const orderId = req.query.id;
+        const order = await orderService.getOrderByOrderId(orderId);
+        res.status(200).json(order);
+    }catch(err){
+        next(err)
+    }
+})
+
+/**
+ * @function GET: /api/order/:userId
+ * @author 채유진
+ * @description 사용자별 주문목록 전체를 조회할 수 있습니다.
+ * @param userId - 사용자 id
+ * @return {object} - code 201, orderList
+ */
 orderRouter.get('/:userId', async (req, res, next) => {
     try {
         const { userId } = req.params;
@@ -29,39 +59,89 @@ orderRouter.get('/:userId', async (req, res, next) => {
 })
 
 /**
- * post, api/order
+ * @function POST: /api/order/:userId
+ * @author 채유진
+ * @description 사용자 주문을 추가할 수 있습니다.
  * @param userId - 사용자 id
- * @param name - 수령인
- * @param address - 수령 주소
- * @param tel - 수령인 연락처
- * @param account - 총액
- * @param orderList - 주문 목록
- * @return object - code 201
+ * @param name - 수령인  ** [req.body]
+ * @param address - 수령 주소  ** [req.body]
+ * @param tel - 수령인 연락처  ** [req.body]
+ * @param account - 총액  ** [req.body]
+ * @param orderList - 주문 목록  ** [req.body]
+ * @return {object} - code 201, newOrder
  */
-// 사용자 주문추가 POST: /api/order
-orderRouter.post('/', async (req, res, next) => {
+orderRouter.post('/:userId', async (req, res, next) => {
     try {
+        // application/json 설정을 프론트에서 안 하면, body가 비어 있게 됨.
+        if (is.emptyObject(req.body)) {
+            throw new Error(
+                "headers의 Content-Type을 application/json으로 설정해주세요"
+            );
+        }
         const { userId, name, address, tel, account, orderList} = req.body
         const newOrder = await orderService.addOrder(userId, name, address, tel, account, orderList);
-        return res.status(201).json(newOrder);
+        res.status(201).json(newOrder);
     }catch(err){
         next(err)
     }
 });
 
+// [사용자 주문수정 필요없음, 삭제만 필요함]
 
-// 관리자 주문조회 GET: /api/order
-orderRouter.get('/', async (req, res) => {
-    const orders = await orderService.getOrderAll();
-    res.status(200).json(orders);
+//
+/**
+ * @function DELETE: api/order/:userId?id=orderId
+ * @author 채유진
+ * @description 사용자가 주문을 취소할 수 있습니다.
+ * @param userId - 사용자 id
+ * @param orderId - 주문 id ** [req.query]
+ * @return {object} - code 201
+ */
+orderRouter.delete('/:userId', async (req, res) => {
+    const { userId } = req.params;
+    const orderId = req.query.id;
+    const result = await orderService.deleteOrder(orderId);
+    res.status(201).json(result);
 })
 
+/**
+ * @function PUT: /api/order/admin?id=orderId
+ * @author 채유진
+ * @description 관리자가 주문상태를 수정할 수 있습니다.
+ * @param orderId - 주문 id  **[req.body]
+ * @param orderState - new주문상태  **[req.body]
+ * @return {object} - code 201
+ */
+orderRouter.put('/admin', async (req, res, next) => {
+    try{
+        // application/json 설정을 프론트에서 안 하면, body가 비어 있게 됨.
+        if (is.emptyObject(req.body)) {
+            throw new Error(
+                "headers의 Content-Type을 application/json으로 설정해주세요"
+            );
+        }
+        const orderId = req.query.id;
+        const { orderState } = req.body;
+        const result = await orderService.updateOrderState(orderId, orderState);
+        res.status(201).json(result);
+    }catch(err){
+        next(err);
+    }
+})
 
-// 주문수정 API
-
-// 사용자 주문취소 API
-
-// 관리자 주문삭제 API
-
+/**
+ * @function DELETE: api/order/admin/:userId?id=orderId
+ * @author 채유진
+ * @description 관리자가 주문을 삭제할 수 있습니다.
+ * @param userId - 사용자 id
+ * @param orderId - 주문 id **[req.query]
+ * @return {object} - code 201
+ */
+orderRouter.delete('/admin:userId', async (req, res) => {
+    const { userId } = req.params;
+    const orderId = req.query.id;
+    const result = await orderService.deleteOrderAdmin(orderId);
+    res.status(201).json(result);
+})
 
 export { orderRouter };
--- src/services/order-service.js
+++ src/services/order-service.js
@@ -27,7 +27,15 @@ class OrderService {
         }
         return myOrders
     }
-
+    async getOrderByOrderId(orderId) {
+        const myOrder = await this.orderModel.findByOrderId(orderId);
+        if (!myOrder) {
+            throw new Error(
+                "주문 내역이 없습니다."
+            );
+        }
+        return myOrder
+    }
 
     // 관리자 주문 조회
     async getOrderAll() {
@@ -40,12 +48,48 @@ class OrderService {
         return orders;
     }
 
-    // 관리자 주문 수정: 배송상태 수정
-    // 사용자 주문 수정: 배송시작 전까지는 주문정보 수정가능
+    // 관리자 주문상태 수정
+    async updateOrderState(orderId, newState) {
+        const result = await orderModel.updateState(orderId, newState);
+        return result;
+    }
 
-    // 주문 취소: 사용자는 개인페이지에서 주문내역 취소가능
-    // 주문 삭제: 관리자는 관리페이지에서 주문내역 삭제가능
+    // 사용자 주문 취소: 사용자는 개인페이지에서 주문내역 취소가능
+    async deleteOrder(orderId) {
 
+        // 사용자의 경우, orderId 기준으로 현재 배송상태 조회
+        const exist = await orderModel.existOrder(orderId)
+        if(exist === 0) {
+            throw new Error(
+                "orderId에 해당하는 주문정보가 없습니다."
+            );
+        }
+        const { orderState } = await orderModel.findByOrderId(orderId);
+
+        if(!orderState) {
+            throw new Error(
+                "orderId에 해당하는 주문정보가 없습니다."
+            );
+        }
+        if (orderState !== '상품 준비중') {
+            throw new Error(
+                "상품 준비가 완료되어, 주문을 취소할 수 없습니다."
+            );
+        }
+        const result = await orderModel.deleteOrder(orderId);
+        return result
+    }
+
+    // 관리자 주문 삭제: 관리자는 관리페이지에서 주문내역 삭제가능
+    async deleteOrderAdmin(orderId) {
+        const result = await orderModel.deleteOrder(orderId);
+        if (result !== true) {
+            throw new Error(
+                "삭제에 실패했습니다. 주문ID를 확인해주세요."
+            );
+        }
+        return result
+    }
 }
 
 const orderService = new OrderService(orderModel);
