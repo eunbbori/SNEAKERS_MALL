348026286823cfecc8fdac0c1e17dd345012c35c
yang <ysotgood@gmail.com>
2022-11-04 17:50:04 +0900

    dev머지수정

---
+++ src/routers/order-router.js
@@ -0,0 +1,45 @@
+import { Router } from "express";
+
+// 폴더에서 import하면, 자동으로 폴더의 index.js에서 가져옴
+import { orderService } from "../services";
+const orderRouter = Router();
+
+// 사용자 주문조회 GET: /api/order/userId
+orderRouter.get('/:userId', async (req, res, next) => {
+    try {
+        const { userId } = req.params;
+        const order = await orderService.getOrderByUserId(userId);
+        res.status(200).json(order);
+    }catch(err){
+        next(err)
+    }
+})
+
+
+// 사용자 주문추가 POST: /api/order
+orderRouter.post('/', async (req, res, next) => {
+    try {
+        const { userId, name, address, tel, account, orderList} = req.body
+        const newOrder = await orderService.addOrder(userId, name, address, tel, account, orderList);
+        return res.status(201).json(newOrder);
+    }catch(err){
+        next(err)
+    }
+});
+
+
+// 관리자 주문조회 GET: /api/order
+orderRouter.get('/', async (req, res) => {
+    const orders = await orderService.getOrderAll();
+    res.status(200).json(orders);
+})
+
+
+// 주문수정 API
+
+// 사용자 주문취소 API
+
+// 관리자 주문삭제 API
+
+
+export { orderRouter };
--- src/routers/product-router.js
+++ src/routers/product-router.js
@@ -1,23 +1,155 @@
 import { Router } from "express";
 import is from "@sindresorhus/is";
 import { loginRequired } from "../middlewares";
+import { productService } from "../services";
 
 const productRouter = Router();
 
 // 상품 저장 api
-productRouter.post('/', loginRequired, async function (req, res, next) {
-    try {
-      // application/json 설정을 프론트에서 안 하면, body가 비어 있게 됨.
-      if (is.emptyObject(req.body)) {
-        throw new Error(
-         "headers의 Content-Type을 application/json으로 설정해주세요"
-        );
-      }
-      res.status(200).json({});
-    } catch (error) {
-      next(error);
+productRouter.post("/", loginRequired, async function (req, res, next) {
+  try {
+    // application/json 설정을 프론트에서 안 하면, body가 비어 있게 됨.
+    if (is.emptyObject(req.body)) {
+      throw new Error(
+        "headers의 Content-Type을 application/json으로 설정해주세요"
+      );
     }
+
+    // req (request)의 body 에서 데이터 가져오기
+    const {
+      code,
+      brand,
+      name,
+      imageUrl,
+      content,
+      category,
+      size,
+      price,
+      stock,
+    } = req.body;
+
+    const newProduct = await productService.addProduct({
+      brand,
+      name,
+      imageUrl,
+      content,
+      category,
+      size,
+      price,
+      stock,
+    });
+
+    res.status(200).json(newProduct);
+  } catch (error) {
+    next(error);
   }
-);
+});
+
+// 상품 리스트 조회 api
+productRouter.get("/", async function (req, res, next) {
+  try {
+    const page = Number(req.query.page || 1);
+    const perPage = 20;
+    const category = req.query.category;
+
+    const { total, products } = await productService.getProductList(
+      page,
+      perPage,
+      category
+    );
+    const totalPage = Math.ceil(total / perPage);
+
+    res.status(200).json({
+      totalPage: totalPage,
+      currentPage: page,
+      totalCount: total,
+      items: products,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 상품 상세 조회 api
+productRouter.get("/:code", async function (req, res, next) {
+  try {
+    const { code } = req.params;
+
+    const productInfo = await productService.getProductDetail(code);
+
+    res.status(200).json(productInfo);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 상품 수정 api
+productRouter.put("/:code", loginRequired, async function (req, res, next) {
+  try {
+    // application/json 설정을 프론트에서 안 하면, body가 비어 있게 됨.
+    if (is.emptyObject(req.body)) {
+      throw new Error(
+        "headers의 Content-Type을 application/json으로 설정해주세요"
+      );
+    }
+    const { code }  = req.params; 
+    const {  
+      brand,
+      name,
+      imageUrl,
+      content,
+      category,
+      size,
+      price,
+      stock } = req.body;
+
+    const { matchedCount } = await productService.updateProduct(
+      code,
+      {
+      brand,
+      name,
+      imageUrl,
+      content,
+      category,
+      size,
+      price,
+      stock
+     }
+    );
+
+    if( matchedCount == 1 ){
+      res.status(200).json({
+        result: true
+      });
+    }else {
+      throw new Error(
+        // "예상치 못한 오류 발생 관리자에게 문의해주세요"
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 상품 삭제 api
+productRouter.delete("/:code", loginRequired, async function (req, res, next) {
+  try {
+    const code = req.params.code; 
+
+    const { deletedCount } = await productService.deleteProduct(code)
+
+    if(deletedCount === 1){
+      res.status(200).json({
+        result: true
+      });
+    }else{
+      throw new Error(
+        "예상치 못한 오류 발생 관리자에게 문의해주세요"
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 export { productRouter };
