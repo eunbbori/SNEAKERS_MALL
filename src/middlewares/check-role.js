d819dfcefeaf01ba84fcb09095264a113ff81aaa
yj0903 <wiseidea01@gmail.com>
2022-11-06 21:28:48 +0900

    사용자/관리자 체크하는 미들웨어

---
+++ src/middlewares/check-role.js
@@ -0,0 +1,27 @@
+import { userService } from "../services";
+
+async function checkRole(req, res, next) {
+	try {
+		const userId = req.currentUserId;
+		console.log('check-role', userId);
+
+		// role 체크
+		const role = await userService.checkRole(userId);
+		if (role !=='admin') {
+			res.status(403).json({
+				result: "forbidden-approach",
+				reason: "관리자 권한이 있는 사용자만 사용할 수 있는 서비스입니다.",
+			});
+		}
+		next();
+	} catch (error) {
+		// jwt.verify 함수가 에러를 발생시키는 경우는 토큰이 정상적으로 decode 안되었을 경우임.
+		// 403 코드로 JSON 형태로 프론트에 전달함.
+		res.status(403).json({
+			result: "forbidden-approach",
+			reason: "접근이 불가능한 페이지입니다.",
+		});
+	}
+}
+
+export { checkRole };
